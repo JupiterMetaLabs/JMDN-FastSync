60e698375db99b563c5eecf593f480024dce5247
Neeraj Chowdary <57310710+neerajchowdary889@users.noreply.github.com>
2026-03-23 18:20:28 +0530

    add: helper functions for pots request and response

---
+++ core/pots/helper/pots_response.go
@@ -0,0 +1,40 @@
+package helper
+
+import (
+	potspb "github.com/JupiterMetaLabs/JMDN-FastSync/common/proto/pots"
+	tagpb "github.com/JupiterMetaLabs/JMDN-FastSync/common/proto/tagging"
+)
+
+type potsresponse_builder struct {
+	PoTSResponse *potspb.PoTSResponse
+}
+
+func NewPoTSResponseBuilder() *potsresponse_builder {
+	return &potsresponse_builder{
+		PoTSResponse: &potspb.PoTSResponse{},
+	}
+}
+
+func (p *potsresponse_builder) AddTag(tags *tagpb.Tag) *potsresponse_builder {
+	p.PoTSResponse.Tag = tags
+	return p
+}
+
+func (p *potsresponse_builder) AddStatus(status bool) *potsresponse_builder {
+	p.PoTSResponse.Success = status
+	return p
+}
+
+func (p *potsresponse_builder) AddErrorMessage(message string) *potsresponse_builder {
+	p.PoTSResponse.ErrorMessage = message
+	return p
+}
+
+func (p *potsresponse_builder) AddLatestBlockNumber(latestBlockNumber uint64) *potsresponse_builder {
+	p.PoTSResponse.LatestBlockNumber = latestBlockNumber
+	return p
+}
+
+func (p *potsresponse_builder) Build() *potspb.PoTSResponse {
+	return p.PoTSResponse
+}
