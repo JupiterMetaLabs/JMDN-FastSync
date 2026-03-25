fff54374ac15d4bba9d55fa77d4c134f87d76598
Neeraj Chowdary <57310710+neerajchowdary889@users.noreply.github.com>
2026-03-25 15:53:10 +0530

    add: functionalities for the pubsub

---
+++ helper/Blocks/block_converter.go
@@ -0,0 +1,84 @@
+package blocks
+
+import (
+	"math/big"
+
+	blockpb "github.com/JupiterMetaLabs/JMDN-FastSync/common/proto/block"
+	"github.com/JupiterMetaLabs/JMDN-FastSync/common/types"
+	ethcommon "github.com/ethereum/go-ethereum/common"
+)
+
+// protoToZKBlock converts a proto block.ZKBlock to a types.ZKBlock.
+// The proto representation uses raw bytes for Ethereum types; this function
+// converts them back to the correct Go types.
+func ProtoToZKBlock(pb *blockpb.ZKBlock) *types.ZKBlock {
+	b := &types.ZKBlock{
+		StarkProof:  pb.GetStarkProof(),
+		Commitment:  pb.GetCommitment(),
+		ProofHash:   pb.GetProofHash(),
+		Status:      pb.GetStatus(),
+		TxnsRoot:    pb.GetTxnsRoot(),
+		Timestamp:   pb.GetTimestamp(),
+		ExtraData:   pb.GetExtraData(),
+		LogsBloom:   pb.GetLogsBloom(),
+		GasLimit:    pb.GetGasLimit(),
+		GasUsed:     pb.GetGasUsed(),
+		BlockNumber: pb.GetBlockNumber(),
+		StateRoot:   ethcommon.BytesToHash(pb.GetStateRoot()),
+		PrevHash:    ethcommon.BytesToHash(pb.GetPrevHash()),
+		BlockHash:   ethcommon.BytesToHash(pb.GetBlockHash()),
+	}
+	if ca := pb.GetCoinbaseAddr(); len(ca) > 0 {
+		addr := ethcommon.BytesToAddress(ca)
+		b.CoinbaseAddr = &addr
+	}
+	if za := pb.GetZkvmAddr(); len(za) > 0 {
+		addr := ethcommon.BytesToAddress(za)
+		b.ZKVMAddr = &addr
+	}
+	b.Transactions = make([]types.Transaction, len(pb.GetTransactions()))
+	for i, tx := range pb.GetTransactions() {
+		b.Transactions[i] = protoToTransaction(tx)
+	}
+	return b
+}
+
+
+// protoToTransaction converts a proto block.Transaction to a types.Transaction.
+func protoToTransaction(tx *blockpb.Transaction) types.Transaction {
+	t := types.Transaction{
+		Hash:           ethcommon.BytesToHash(tx.GetHash()),
+		Type:           uint8(tx.GetType()),
+		Timestamp:      tx.GetTimestamp(),
+		Nonce:          tx.GetNonce(),
+		GasLimit:       tx.GetGasLimit(),
+		Data:           tx.GetData(),
+		Value:          bytesToBigInt(tx.GetValue()),
+		ChainID:        bytesToBigInt(tx.GetChainId()),
+		GasPrice:       bytesToBigInt(tx.GetGasPrice()),
+		MaxFee:         bytesToBigInt(tx.GetMaxFee()),
+		MaxPriorityFee: bytesToBigInt(tx.GetMaxPriorityFee()),
+		V:              bytesToBigInt(tx.GetV()),
+		R:              bytesToBigInt(tx.GetR()),
+		S:              bytesToBigInt(tx.GetS()),
+	}
+	if from := tx.GetFrom(); len(from) > 0 {
+		addr := ethcommon.BytesToAddress(from)
+		t.From = &addr
+	}
+	if to := tx.GetTo(); len(to) > 0 {
+		addr := ethcommon.BytesToAddress(to)
+		t.To = &addr
+	}
+	return t
+}
+
+
+// bytesToBigInt converts a big-endian byte slice to *big.Int.
+// Returns nil for empty/nil slices (proto zero value).
+func bytesToBigInt(b []byte) *big.Int {
+	if len(b) == 0 {
+		return nil
+	}
+	return new(big.Int).SetBytes(b)
+}
